1b299e26e2dbd2ea952c5b2e5da7280a7dd16819
navga228 <novgorodov74@bk.ru>
2023-09-14 16:09:17 +0300

    feat: business entities modules, swagger, typeorm, handlebars packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentModule } from './comment/comment.module';
+import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [CommentModule, PostModule, CategoryModule, UserModule, BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
+import * as hbs from 'express-handlebars';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  app.engine('hbs', hbs({
+    extname: 'hbs',
+    partialsDir: join(__dirname, '..', 'views/partials'),
+    layoutsDir: join(__dirname, '..', 'views/layouts'),
+    defaultLayout: 'main',
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Navga Blog Service')
+    .setDescription('Блоги, посты, категории & комменты')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
